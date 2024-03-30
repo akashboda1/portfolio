fa8ed152c5c4f9d1292a8a6664c19a9c38ed986d
Akash Boda <contact@akashboda.com>
2024-03-30 16:21:27 -0400

    updated author and contact page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,8 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/contact', title: 'Contact' },
 ]
 
 export default headerNavLinks
